cebe70c61acbf7e8e94111b2555d841bb21e7e9e
Kevin Chang <kckaiwei@gmail.com>
2016-11-21 14:09:26 -0600

    reading table data to datagrid display

--- SQLViewer/SQLViewer/editor_window.xaml.cs
+++ SQLViewer/SQLViewer/editor_window.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace SQLViewer
 {
@@ -57,7 +58,38 @@ namespace SQLViewer
 
         private void table_comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (table_comboBox.SelectedIndex > -1)
+            {
+                DataTable dataTable = new DataTable();
 
+                try
+                {
+                    string sql_command = "SELECT * FROM ";
+                    sql_command = sql_command + table_comboBox.SelectedItem.ToString();
+                    conn = new MySql.Data.MySqlClient.MySqlConnection();
+                    conn.ConnectionString = myConnectionString;
+                    conn.Open();
+                    MySqlCommand command = conn.CreateCommand();
+                    command.CommandText = sql_command;
+
+                    //Load data into dataTable object, which can then be imported to dataGrid
+                    using (MySqlDataAdapter da = new MySqlDataAdapter(command))
+                    {
+                        da.Fill(dataTable);
+                    }
+
+                    dataGrid.DataContext = dataTable;
+                    dataGrid.Name = dataTable.TableName;
+                    conn.Close();
+
+                }
+                catch (MySql.Data.MySqlClient.MySqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                //Debugging
+                //MessageBox.Show(table_comboBox.SelectedItem.ToString());
+            }
         }
     }
 
