e73cf3ec8645487edf5f350e4bab1c795f5cf071
Kevin Chang <kckaiwei@gmail.com>
2016-11-21 12:25:51 -0600

    adds all tables into dropdown

---
+++ SQLViewer/SQLViewer/sql_object.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLViewer
+{
+    public class sql_object
+    {
+        public string login { get; set; }
+        public string uri { get; set; }
+        public string password { get; set; }
+        public string database { get; set; }
+    }
+}
